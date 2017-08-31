05e20ace0e10a42dce0aa7c4487791a8df7c9091
Arockiyajoicy123456789 <31499233+Arockiyajoicy123456789@users.noreply.github.com>
2017-08-31 19:24:26 +0530

    Create Leapyear.c

---
+++ Leapyear.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+int year;
+printf("Enter the year");
+scanf("%d",&year");
+if(year%4==0)
+{
+printf("THE YEAR IS LEAP YEAR");
+}
+else
+{
+printf("THE YEAR IS NOT LEAP YEAR");
+}
+}
