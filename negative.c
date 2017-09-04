56b24b3ec71cb7211baa16181a8d9ab6cd07d3b4
Arockiyajoicy123456789 <31499233+Arockiyajoicy123456789@users.noreply.github.com>
2017-09-04 19:14:21 +0530

    Create negative.c

---
+++ negative.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int main()
+{
+int number;
+printf("Enter the number");
+scanf("%d",&number);
+if(number>0)
+{
+printf("%d is positive");
+}
+if(number<0)
+{
+printf("%d is negative");
+}
+if(number==0)
+{
+printf("%d is zero");
+}
+return 0;
+}
