c599349c349152ef0b2ac1d9403de02008a49a7e
Arockiyajoicy123456789 <31499233+Arockiyajoicy123456789@users.noreply.github.com>
2017-09-10 16:02:33 +0530

    Create j.c

---
+++ j.c
@@ -0,0 +1,35 @@
+#include<stdio.h>
+int main()
+{
+int a,b,i,temp1,temp2,result=0,remainder,n=0;
+printf("Enter two numbers(intervals):");
+scanf("%d %d",&a,&b);
+printf("Armstrond number between %d and %d are:",a,b);
+for(i=low+1;i<high;++i)
+{
+temp2=i;
+temp1=i;
+while(temp1!=0)
+{
+temp1/=10;
+++n;
+}
+while(temp2!=0)
+{
+remainder=temp2%10;
+result+=pow(remainder,n);
+temp2/=10;
+}
+if(result==i)
+{
+printf("%d",i);
+}
+n=o;
+result=0;
+}
+return 0;
+}
+
+
+
+
