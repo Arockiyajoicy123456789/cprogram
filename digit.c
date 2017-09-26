c54d09ed9a48fe6d9db68e7205874a1999229409
Arockiyajoicy123456789 <31499233+Arockiyajoicy123456789@users.noreply.github.com>
2017-09-26 19:04:35 +0530

    Create digit.c

---
+++ digit.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+int number,reversenumber=0,digit;
+printf("Enter the number:");
+scanf("%d",&number);
+while(number!=0)
+{
+digit=number%10;
+reversenumber=reversenumber*10+digit;
+number/=10;
+}
+printf("Revese number=%d",reversenumber);
+return 0;
+}
+
