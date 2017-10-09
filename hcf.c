f34600e962f449867611be76460ca6ac6c0317b4
Arockiyajoicy123456789 <31499233+Arockiyajoicy123456789@users.noreply.github.com>
2017-10-09 19:08:04 +0530

    Create hcf.c

---
+++ hcf.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+int i,min,num1,num2,hcf=1;
+printf("Enter any two numbe to find HCF:");
+scanf("%d%d",&num1,&num2);
+min=(num1<num2)?num1:num2;
+for(i=1;i<=min;i++)
+{
+if(num1%i==0&&num2%i==0)
+{
+hcf=1;
+}
+}
+printf("HCF OF %d and %d=%d\n",num1,num2,hcf);
+return 0;
+}
