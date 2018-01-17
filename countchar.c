4ef5ab28887011f48c2878efea2ce1484470ec40
Arockiyajoicy123456789 <31499233+Arockiyajoicy123456789@users.noreply.github.com>
2018-01-17 19:27:29 +0530

    Create countchar.c

---
+++ countchar.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+ int i;
+ int count=0;
+ char str[20];
+ printf("Enter string : ");
+ scanf("%s",&str);
+ for(i=0; str[i]!=NULL; i++)
+ {
+   count++;
+  }
+ printf("\nNumber of characters in string : %d",count);
+ return 0;
+}
