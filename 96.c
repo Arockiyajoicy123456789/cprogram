845f40e4158a7519b2d711dfe3753079c3e5563c
Arockiyajoicy123456789 <31499233+Arockiyajoicy123456789@users.noreply.github.com>
2018-02-20 19:10:24 +0530

    Create 96.c

---
+++ 96.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+void main()
+{
+  int n,i;
+  scanf("%d",&n);
+  for(i=2;i<n;i++)
+  {
+    if(n%i==0)
+    {
+      break;
+    }
+  }
+    if(n!=i)
+    {
+      printf("yes");
+    }
+    else
+    {
+      printf("No");
+    }
+    
+  
+}
